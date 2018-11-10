b6579095de0386d464a23f6bedde2d388206dec1
Ethan Simpson <ethansown@gmail.com>
2018-11-10 17:31:06 -0600

    Fixed Collapsibles

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ function collapsible(){
     //Run each index of the 'collapsible' collection of elements through a for loop//
     for(i = 0; i < collapsible.length; i++){
         //Add Event Listeners to all 'collapsible' elements//
-        collapsible[i].addEventListener("click", function(){
-            var content = this.firstElementChild;
-            if(content.style.display === "block"){
-                content.style.display = "none";
+        collapsible[i].children[0].addEventListener("click", function(){
+            var content = this.nextElementSibling;
+            if(content.style.maxHeight){
+                content.style.maxHeight = null;
             } else {
-                content.style.display = "block";
+                content.style.maxHeight = content.scrollHeight +"px";
             }
         })
     }
