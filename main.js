f4aae2012f99ced9beef386cb4872346bed5a5ba
Ethan Simpson <ethansown@gmail.com>
2018-11-10 19:31:23 -0600

    Nav now Responsive for Mobile

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 /*Collapsible Start*/
-
 function collapsible(){
     //Grab all elements with class name "collapsible"//
     const collapsible = document.getElementsByClassName("collapsible");
@@ -16,6 +15,35 @@ function collapsible(){
             }
         })
     }
-    
+}
+function media(){
+    function matchScreen(screen){
+        if(screen.matches){
+            var nav = document.querySelector("nav");
+            var navList = document.getElementById("navList");
+            var navLinks = nav.querySelectorAll("a");
+            var navIconArray = ['<i class="fas fa-home"></i>', '<i class="fas fa-question"></i>', '<i class="fas fa-folder"></i>', '<i class="fas fa-at"></i>'];
+            nav.style.position = "fixed";
+            nav.style.left = 0;
+            nav.style.width = "50px";
+            nav.style.height= "100%";
+            navList.style.display = "flex";
+            navList.style.flexDirection = "column";
+            navList.style.justifyContent = "flex-start";
+            for(i = 0; i < navLinks.length; i++){
+                navLinks[i].style.padding = "10px 0 10px 0";
+                navLinks[i].style.fontSize = "1.8em";
+                navLinks[i].children[0].style.display = "inline-block";
+                navLinks[i].children[0].innerHTML = navIconArray[i];
+            }
+        } else{
+            document.body.style.backgroundColor = "white";
+        }
+    }
+console.log("I am running");
+var screen = window.matchMedia("(max-width: 700px)");
+matchScreen(screen);
+screen.addListener(matchScreen);
 }
 window.addEventListener("load", collapsible());
+window.addEventListener("load", media());
